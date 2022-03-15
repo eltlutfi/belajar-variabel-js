05469eaadc8fe2f11c147cf230b4fbb1e669eded
eltlutfi <lutfi200499@gmail.com>
2022-03-16 02:43:28 +0700

    belajar variabel js

---
+++ main.js
@@ -0,0 +1,7 @@
+// String
+const folder = "catatan"
+const file = "tools & website"
+
+// String-TemplateLiteral
+console.log(`ini adalah folder ${folder} , yang berisi file ${file} sebagai pendukung untuk Web Developer`)
+
