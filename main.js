f08b019f2a334fc3f241f411c05b9da0192338bd
eltlutfi <lutfi200499@gmail.com>
2022-03-16 02:57:37 +0700

    menambahkan number pada js

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ const file = "tools & website"
 // String-TemplateLiteral
 console.log(`ini adalah folder ${folder} , yang berisi file ${file} sebagai pendukung untuk Web Developer`)
 
+// Number
+let nilaiKegunaanCatatan = 60;
+nilaiKegunaanCatatan = 88.5;
+console.log(nilaiKegunaanCatatan);
+
