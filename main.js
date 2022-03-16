dfe28cec94b0aca04ff12f7d6b0828a656f28732
eltlutfi <lutfi200499@gmail.com>
2022-03-16 22:30:19 +0700

    menambahkan boolean

--- main.js
+++ main.js
@@ -2,11 +2,19 @@
 const folder = "catatan"
 const file = "tools & website"
 
-// String-TemplateLiteral
+// Template Literal
 console.log(`ini adalah folder ${folder} , yang berisi file ${file} sebagai pendukung untuk Web Developer`)
 
 // Number
 let nilaiKegunaanCatatan = 60;
 nilaiKegunaanCatatan = 88.5;
 console.log(nilaiKegunaanCatatan);
+
+// Boolean
+const bagiSayaCatatanIniPenting = true;
+const bagiSayaCatatanIniTidakPenting = false;
+
+console.log (bagiSayaCatatanIniPenting);
+console.log (bagiSayaCatatanIniTidakPenting);
+
 
